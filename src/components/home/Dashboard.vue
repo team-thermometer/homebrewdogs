f5130adb06750bc74453b12d8c88e3b745dcd672
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-17 14:51:20 -0800

    base dashboard component minus table

---
+++ src/components/home/Dashboard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>I am the dashboard component</h1>
+    <Beer v-for="beer in beers"
+            :key="beer.id"
+            :beer="beer"/>
+    </div>
+</template>
+
+<script>
+import Beer from '../beers/Beer';
+export default {
+  props: {
+    beer: Object
+  },
+  components: {
+    Beer
+  }
+};
+</script>
+
+<style>
+
+</style>
