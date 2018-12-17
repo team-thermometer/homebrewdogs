90ccc641842440305f1a1b9d417519d8ea0a9530
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-17 14:17:50 -0800

    header links added

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,14 +1,17 @@
 <template>
   <header>
       <RouterLink to="/"><img src="../../assets/logo.png" alt="homebrewdog logo" class="logo"></RouterLink>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-    </nav>
   </header>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      result: null,
+      user: null,
+    };
+  },
 };
 </script>
 
