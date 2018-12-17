f41168bc99ddf97ec7b863e6dc2106044316e5c5
avanvalk <drewvv@gmail.com>
2018-12-17 14:35:14 -0800

    added Beers.vue

---
+++ src/components/beers/Beers.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+      <div>
+      <ul>
+          Beers.vue
+      </ul>
+    </div>
+  </section>
+</template>
+
+<script>
+//import Beer from './Beer';
+// export default {
+//   components: {
+//     Beer
+//   }
+//};
+</script>
