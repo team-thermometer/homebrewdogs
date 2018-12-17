00fd41a9a58f52c1280dcdcd3aa90e9c3654dd32
avanvalk <drewvv@gmail.com>
2018-12-17 15:25:21 -0800

    updated beers

--- src/router.js
+++ src/router.js
@@ -2,16 +2,16 @@ import VueRouter from 'vue-router';
 import Home from './components/home/Home';
 import About from './components/About';
 import Beers from './components/beers/Beers';
-import BeerDetail from './components/beers/BeerDetail';
-import Dashboard from './components/home/Dashboard';
+// import BeerDetail from './components/beers/BeerDetail';
+// import Dashboard from './components/home/Dashboard';
 
 export default new VueRouter({
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About },
     { path: '/beers', component: Beers },
-    { path: '/dashboard', component: Dashboard },
-    { path: '/beers/:id', component: BeerDetail },
+    // { path: '/dashboard', component: Dashboard },
+    // { path: '/beers/:id', component: BeerDetail },
     { path: '*', redirect: '/' }
   ]
 });
