2610ed73a139fb1dd6edb00f55a7e35c73ebd94a
Dyanna Tolman <dyannast@gmail.com>
2018-12-17 14:32:41 -0800

    beer search file

---
+++ src/components/beers/BeerSearch.vue
@@ -0,0 +1,40 @@
+<template>
+    <form @submit.prevent="handleSubmit">
+        <input v-model="keyword">
+        <button>Search</button>
+    </form>
+</template>
+
+<script>
+export default {
+  props: {
+    search: String,
+    onSearch: Function
+  },
+  data() {
+    return {
+      keyword: this.search || ''
+    };
+  },
+  watch: {
+    search(newSearch) {
+      if(this.keyword !== newSearch) {
+        this.keyword = newSearch;
+      }
+    }
+  },
+  methods: {
+    handleSubmit() {
+      this.$router.push({
+        query: {
+          search: encodeURIComponent(this.keyword)
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
