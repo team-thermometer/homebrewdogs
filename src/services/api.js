7b788f960f08320b024cd0c9eed71d8a834a150a
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-17 13:37:48 -0800

    sign in and up

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,58 @@
+let token = '';
+
+const getOptions = (method, data) => {
+  const options = {
+    method,
+    headers: {}
+  };
+
+  if(data) {
+    options.headers['Content-Type'] = 'application/json';
+    options.body = JSON.stringify(data);
+  }
+
+  if(token) {
+    options.headers.Authorization = token;
+  }
+
+  return options;
+};
 
 export default {
+  
   test() {
     return fetch('https://api.punkapi.com/v2/beers')
       .then(response => response.json());
-  }
+  },
+  setToken(t) {
+    token = t;
+  },
+
+  signUp(profile) {
+    return fetch('/api/auth/signup', getOptions('POST', profile))
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+
+        return response.json()
+          .then(error => {
+            return Promise.reject(error);
+          });
+      });
+  },
+
+  signIn(credentials) {
+    return fetch('/api/auth/signin', getOptions('POST', credentials))
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+
+        return response.json()
+          .then(error => {
+            return Promise.reject(error);
+          });
+      });
+  },
 };
