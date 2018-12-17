361639097b0f1bbc0346ea2e56296c0ff18fcd17
Aaron Dennis <AMDennis1987@gmail.com>
2018-12-17 14:31:47 -0800

    front end routes for pages

--- src/router.js
+++ src/router.js
@@ -1,14 +1,17 @@
 import VueRouter from 'vue-router';
 import Home from './components/home/Home';
 import About from './components/About';
-// import Beers from './components/beers/Beers';
+import Beers from './components/beers/Beers';
+import BeerDetail from './components/beers/BeerDetail';
+import Dashboard from './components/home/Dashboard';
 
 export default new VueRouter({
-  // mode: 'history',
   routes: [
     { path: '/', component: Home },
     { path: '/about', component: About },
-    // { path: '/beers', component: Beers },
+    { path: '/beers', component: Beers },
+    { path: '/dashboard', component: Dashboard },
+    { path: '/beers/:id', component: BeerDetail },
     { path: '*', redirect: '/' }
   ]
 });
