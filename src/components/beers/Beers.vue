7de584516cd73173da03cf3da622045193c3674b
Dyanna Tolman <dyannast@gmail.com>
2018-12-17 17:52:56 -0800

    getting close to connecting modal with search component

--- src/components/beers/Beers.vue
+++ src/components/beers/Beers.vue
@@ -3,21 +3,39 @@
     <Beer v-for="beer in beers"
       :key="beer.id"
       :beer="beer"/>
+    <p>
+    <button @click="showModal = true"> Search
+    </button>
+    </p>
+    <Modal v-if="showModal" :onClose="() => showModal = false">
+      <BeerSearch :onSearch="handleSearch"/>
+    </Modal>
   </ul>
 </template>
 
 <script>
 import api from '../../services/api';
 import Beer from './Beer';
+import Modal from '../shared/Modal';
+import BeerSearch from './BeerSearch';
 
 export default {
   data() {
     return {
-      beers: null
+      beers: null,
+      showModal: false
     };
   },
   components: {
-    Beer
+    Beer,
+    BeerSearch,
+    Modal
+  },
+  methods: {
+    handleSearch(beer) {
+      return this.onSearch(beer)
+        .then(() => this.show = false);
+    }
   },
   created() {
     api.getBeers()
