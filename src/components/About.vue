20370ca04bcd1c632a2f54544eca8ee6a5e7bff9
Dyanna Tolman <dyannast@gmail.com>
2018-12-18 15:29:59 -0800

    more aboutus with bio and pics

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,7 +1,37 @@
 <template>
     <div>
         <h1>About Us</h1>
-    <p>fdsfdsfdsfds</p>
+    <section>
+    <h3>Cari Palazzolo</h3>
+    <img src= "../assets/caripalazzolo.jpg">
+    <p>
+        Cari is a full-stack developer currently training at Alchemy Code Lab. She has backgrounds in QA/QC, Graphic Design, Visual Art, and Music. 
+        When she’s not writing code she enjoys biking around Portland, going on hikes and sipping delicious IPA’s. 
+        Her favorite Brewdog beer is the Punk IPA.
+    </p>
+    </section>
+    <section>
+    <h3>Dyanna "Dee" Tolman</h3>
+    <!-- <img src= "../assets/profile-pic.jpg"> -->
+    <p>
+        Dyanna is currently a student at Alchemy Code Lab and has lived in Portland on and off for 10 years. 
+        Originally from Singapore, she enjoys traveling, trying new restaurants and spending time with her family.
+        She doesn't drink but if she did, she thinks her favorite Brewdog beer would be Electric India.
+    </p>
+    </section>
+    <section>
+    <h3>Aaron Dennis</h3>
+    <img src= "../assets/AboutMePic.jpeg">
+    <p>Aaron is a climber, traveler, software developer, and amateur chef. He has actually visited the BrewDog main brewery in Edinburgh, Scotland. His favorite BrewDog beer is Punk IPA.
+    </p>
+    </section>
+    <section>
+    <h3>Drew Van Valkenburg</h3>
+    <img src= "../assets/profile_pic.jpg">
+    <p>Drew is an avid reader, web developer, economist, snowboarder and basketball player.</p>
+    <p>
+    </p>
+    </section>
     </div>
 </template>
 
