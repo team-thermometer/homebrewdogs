6ee0372e2321618178c0a7a32a2b499689545da6
Dyanna Tolman <dyannast@gmail.com>
2018-12-18 16:39:15 -0800

    all pics posted

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,9 +1,9 @@
 <template>
-    <div>
+    <div class="aboutus">
         <h1>About Us</h1>
     <section>
     <h3>Cari Palazzolo</h3>
-    <img src= "../assets/caripalazzolo.jpg">
+    <img class="profile" src= "../assets/caripalazzolo.jpg">
     <p>
         Cari is a full-stack developer currently training at Alchemy Code Lab. She has backgrounds in QA/QC, Graphic Design, Visual Art, and Music. 
         When she’s not writing code she enjoys biking around Portland, going on hikes and sipping delicious IPA’s. 
@@ -12,7 +12,7 @@
     </section>
     <section>
     <h3>Dyanna "Dee" Tolman</h3>
-    <img src= "../assets/profile.jpg">
+    <img class="profile" src= "../assets/profile.jpg">
     <p>
         Dyanna is currently a student at Alchemy Code Lab and has lived in Portland on and off for 10 years. 
         Originally from Singapore, she enjoys traveling, trying new restaurants and spending time with her family.
@@ -21,14 +21,14 @@
     </section>
     <section>
     <h3>Aaron Dennis</h3>
-    <img src= "../assets/AboutMePic.jpeg">
+    <img class="profile" src= "../assets/AboutMePic.jpeg">
     <p>Aaron is a climber, traveler, software developer, and amateur chef. He has actually visited the BrewDog main brewery in Edinburgh, Scotland. His favorite BrewDog beer is Punk IPA.
     </p>
     </section>
     <section>
     <h3>Drew Van Valkenburg</h3>
-    <img src= "../assets/profile_pic.jpg">
-    <p>Drew is an avid reader, web developer, economist, snowboarder and basketball player.</p>
+    <img class="profile" src= "../assets/profile_pic.jpg">
+    <p>Drew is an avid reader, web developer, economist, snowboarder and basketball player. Drew's favorite beer is Fresh Squeezed IPA</p>
     <p>
     </p>
     </section>
@@ -36,8 +36,16 @@
 </template>
 
 <style>
-img {
- width: 200px;
-    height: 120px;
+.aboutus {
+    font-size: 20px;
+    margin: 10px;
+}
+.profile {
+width: 200px;
+height: auto;
+}
+p {
+    padding: 40px;
+    margin: 20px;
 }
 </style>
